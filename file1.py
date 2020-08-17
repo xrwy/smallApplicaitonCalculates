dc49720963871c91e364c20d3038d775ae17dcfa
xrwy <69810869+xrwy@users.noreply.github.com>
2020-08-17 19:50:28 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,5 +1,15 @@
-deger = ["furkan","eray"]  
+import os
 
-with open("d","w") as f:
-    f.readlines(deger) 
-    print(f.read())  
+def yazdir(): 
+    with open("d.txt","w") as f:
+        f.writelines(["furkan \n","eray\n","Ömer\n","frkn123 \n","ery893243 \n","f32pw862 \n","ffesj\n","sfjbsfsef\n","7y84395 \n","89765^+' \n"]) 
+        
+yazdir()   
+
+def okuma():
+    if os.path.isfile("d.txt"):
+        with open("d.txt","r") as yazdir:
+            print(yazdir.read())   
+
+okuma()        
+
